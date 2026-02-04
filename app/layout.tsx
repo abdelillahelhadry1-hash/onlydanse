893cc567512a6c1df82fd294679405e26a3850b5
abdelillahelhadry1-hash <abdelillahelhadry1@gmail.com>
2026-02-04 00:24:19 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
 import Header from "@/app/components/Header";
+import SearchBar from "@/app/components/SearchBar";
+import CategoryNav from "@/app/components/CategoryNav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +31,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50`}
       >
+        {/* Global Header */}
         <Header />
+
+        {/* Airbnb-style Search Bar */}
+        <SearchBar />
+
+        {/* Category Navigation */}
+        <CategoryNav />
+
+        {/* Page Content */}
         <main className="pt-10 px-4">
           {children}
         </main>
