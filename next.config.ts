bb81b11d5230d0dc51707cd90dc07b63a7343808
abdelillahelhadry1-hash <abdelillahelhadry1@gmail.com>
2026-02-22 11:57:12 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 
-const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.supabase.co"
+      },
+      {
+        protocol: "https",
+        hostname: "onlydanse.com"
+      }
+    ]
+  },
+
+  experimental: {
+    serverActions: true
+  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
